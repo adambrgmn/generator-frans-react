cbca20e68c492d1f6e008d522d332d49713c50f2
Adam Bergman <adam@fransvilhelm.com>
2016-10-18 23:38:41 +0200

    INITIAL setup

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,14 +1,25 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
 
-module.exports = yeoman.Base.extend({
-  prompting: function () {
+var _yeomanGenerator = require('yeoman-generator');
+
+var _yeomanGenerator2 = _interopRequireDefault(_yeomanGenerator);
+
+var _chalk = require('chalk');
+
+var _chalk2 = _interopRequireDefault(_chalk);
+
+var _yosay = require('yosay');
+
+var _yosay2 = _interopRequireDefault(_yosay);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = _yeomanGenerator2.default.Base.extend({
+  prompting: function prompting() {
+    var _this = this;
+
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the classy ' + chalk.red('generator-frans-react') + ' generator!'
-    ));
+    this.log((0, _yosay2.default)('Welcome to the classy ' + _chalk2.default.red('generator-frans-react') + ' generator!'));
 
     var prompts = [{
       type: 'confirm',
@@ -19,18 +30,13 @@ module.exports = yeoman.Base.extend({
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
+      _this.props = props;
+    });
   },
-
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+  writing: function writing() {
+    this.fs.copy(this.templatePath('dummyfile.txt'), this.destinationPath('dummyfile.txt'));
   },
-
-  install: function () {
+  install: function install() {
     this.installDependencies();
   }
 });
---
+++ src/app/index.js
@@ -0,0 +1,37 @@
+import yeoman from 'yeoman-generator';
+import chalk from 'chalk';
+import yosay from 'yosay';
+
+
+module.exports = yeoman.Base.extend({
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      `Welcome to the classy ${chalk.red('generator-frans-react')} generator!`
+    ));
+
+    const prompts = [{
+      type: 'confirm',
+      name: 'someAnswer',
+      message: 'Would you like to enable this option?',
+      default: true,
+    }];
+
+    return this.prompt(prompts)
+      .then((props) => {
+        // To access props later use this.props.someAnswer;
+        this.props = props;
+      });
+  },
+
+  writing() {
+    this.fs.copy(
+      this.templatePath('dummyfile.txt'),
+      this.destinationPath('dummyfile.txt')
+    );
+  },
+
+  install() {
+    this.installDependencies();
+  },
+});
