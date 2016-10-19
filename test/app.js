9cc1058456ec72bfe7ede3775d6440503d9038f6
Adam Bergman <adam@fransvilhelm.com>
2016-10-19 02:27:37 +0200

    UPDATED tests

--- test/app.js
+++ test/app.js
@@ -1,17 +1,41 @@
-'use strict';
-
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+import { join } from 'path';
+import assert from 'yeoman-assert';
+// import { expect } from 'chai';
+import helpers from 'yeoman-test';
+// import fs from 'fs-extra';
 
 describe('generator-frans-react:app', () => {
-  before(() => helpers.run(path.join(__dirname, '../src/app'))
-    .withPrompts({ someAnswer: true })
-    .toPromise());
+  before(function before() {
+    this.answers = {
+      name: 'my app',
+    };
 
-  it('creates files', () => {
+    return helpers.run(join(__dirname, '../src/app'))
+      .withPrompts(this.answers)
+      .toPromise();
+  });
+
+  it('should create files', () => {
     assert.file([
-      'dummyfile.txt',
+      '.babelrc',
+      '.editorconfig',
+      '.eslintrc',
+      '.gitignore',
+      'LICENSE',
+      'README.md',
+      'webpack.config.babel.js',
     ]);
+  });
+
+  it('should create a package.json file', () => {
+    assert.file('package.json');
+    assert.jsonFileContent('package.json', {
+      name: 'my-app',
+      version: '0.0.0',
+      description: '',
+      homepage: '',
+      repository: '',
+      author: '',
+    });
   });
 });
