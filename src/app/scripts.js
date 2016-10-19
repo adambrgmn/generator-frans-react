c33165514bb1b7b90ff6018cc1ad0db894ca7d47
Adam Bergman <adam@fransvilhelm.com>
2016-10-20 01:17:10 +0200

    ADDED deploy script

--- src/app/scripts.js
+++ src/app/scripts.js
@@ -4,4 +4,5 @@ export default {
   'build:stats': 'webpack --profile --json > stats.json',
   test: 'tape-watch -1 -r ../test/setup.js \'test/**/*.spec.js\' -o \'| tap-spec --color\'',
   'test:watch': 'npm run test -- --watch',
+  deploy: 'gh-pages -d build',
 };
