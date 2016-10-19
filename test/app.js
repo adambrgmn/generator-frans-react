40fe34525730c1ba10b4ba16acd68c65b2c2665a
Adam Bergman <adam@fransvilhelm.com>
2016-10-20 00:20:26 +0200

    UPDATED tests

--- test/app.js
+++ test/app.js
@@ -1,13 +1,18 @@
 import { join } from 'path';
 import assert from 'yeoman-assert';
-// import { expect } from 'chai';
 import helpers from 'yeoman-test';
-// import fs from 'fs-extra';
 
 describe('generator-frans-react:app', () => {
   before(function before() {
     this.answers = {
-      name: 'my app',
+      name: 'my-app',
+      description: 'A React app',
+      homepage: 'http://fransvilhelm.com',
+      githubAccount: 'adambrgmn',
+      authorName: 'Adam Bergman',
+      authorEmail: 'adam@fransvilhelm.com',
+      authorUrl: 'http://fransvilhelm.com',
+      keywords: ['foo', 'bar'],
     };
 
     return helpers.run(join(__dirname, '../src/app'))
@@ -15,15 +20,64 @@ describe('generator-frans-react:app', () => {
       .toPromise();
   });
 
-  it('should create a package.json file', () => {
-    assert.file('package.json');
+  it('should create static files', () => {
+    assert.file([
+      '.babelrc',
+      '.editorconfig',
+      '.eslintrc',
+      '.gitignore',
+      'webpack/index.ejs',
+      'webpack/parts.js',
+      'webpack/paths.js',
+      'src/index.js',
+      'src/styles.scss',
+      'src/styles/_variables.scss',
+      'test/setup.js',
+      'test/components/App.spec.js',
+    ]);
+  });
+
+  it('should create a package.json file', function it() {
+    const { answers } = this;
+
+    assert.file(['package.json']);
     assert.jsonFileContent('package.json', {
-      name: 'my-app',
+      name: answers.name,
       version: '0.0.0',
-      description: '',
-      homepage: '',
-      repository: '',
-      author: '',
+      description: answers.description,
+      homepage: answers.homepage,
+      repository: `${answers.githubAccount}/${answers.name}`,
+      author: {
+        name: answers.authorName,
+        email: answers.authorEmail,
+        url: answers.authorUrl,
+      },
+      keywords: this.answers.keywords,
+      main: 'src/index.js',
     });
+  });
+
+  it('should create README.md', function it() {
+    const { name } = this.answers;
+
+    assert.file('README.md');
+    assert.fileContent('README.md', `# ${name}`);
+  });
+
+  it('should create LICENSE', function it() {
+    const { authorName, authorEmail, authorUrl } = this.answers;
+    assert.file('LICENSE');
+    assert.fileContent('LICENSE', `Copyright (c) 2016 ${authorName} <${authorEmail}> (${authorUrl})`);
+  });
+
+  it('should create webpack.config.babel.js', function it() {
+    const { name } = this.answers;
+    assert.file('webpack.config.babel.js');
+    assert.fileContent('webpack.config.babel.js', `title: '${name}',`);
+  });
+
+  it('should setup Git', () => {
+    assert.file(['.gitignore', '.gitattributes', '.git']);
+    assert.fileContent('.gitignore', 'build\nstats.json');
   });
 });
