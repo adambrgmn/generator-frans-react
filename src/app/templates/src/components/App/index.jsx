96db6a9ec4ef1809ce02aac8736ef5afab4713e7
Adam Bergman <adam@fransvilhelm.com>
2016-10-20 00:20:06 +0200

    ADDED templates

---
+++ src/app/templates/src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react';
+import styles from 'styles.scss';
+
+export default function App({ name }) {
+  return (
+    <h1 className={styles.header}>
+      Hello {name}!
+    </h1>
+  );
+}
+
+App.propTypes = {
+  name: PropTypes.string.required,
+};
