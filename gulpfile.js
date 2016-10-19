4aee8b49aa5ccbefb84846ba41d39a046271885d
Adam Bergman <adam@fransvilhelm.com>
2016-10-20 00:18:07 +0200

    UPDATED gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('./test/setup');
 const path = require('path');
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
@@ -10,7 +11,7 @@ const nsp = require('gulp-nsp');
 const del = require('del');
 const plumber = require('gulp-plumber');
 
-gulp.task('static', () => gulp.src(['**/*.js', '!src/**/templates/**/*.js'])
+gulp.task('lint', () => gulp.src(['**/*.js', '!src/**/templates/**/*.js'])
   .pipe(excludeGitignore())
   .pipe(eslint())
   .pipe(eslint.format())
@@ -25,10 +26,7 @@ gulp.task('test', (cb) => {
 
   gulp.src('test/**/*.js')
     .pipe(plumber())
-    .pipe(mocha({
-      reporter: 'spec',
-      require: './test/setup',
-    }))
+    .pipe(mocha({ reporter: 'spec' }))
     .on('error', (err) => {
       mochaErr = err;
     })
