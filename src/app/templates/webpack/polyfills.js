3ec14497634f3975e3b4e7bc4915632f8d466270
Adam Bergman <adam@fransvilhelm.com>
2016-10-24 10:41:45 +0200

    ADDED polyfills

---
+++ src/app/templates/webpack/polyfills.js
@@ -0,0 +1,9 @@
+/* eslint-disable global-require */
+
+if (typeof Promise === 'undefined') {
+  require('promise/lib/rejection-tracking').enable();
+  window.Promise = require('promise/lib/es6-extensions');
+}
+
+require('whatwg-fetch');
+Object.assign = require('object-assign');
