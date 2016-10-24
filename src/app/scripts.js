591d2413cd0dfed4f8cb05eaad772f7f296e224e
Adam Bergman <adam@fransvilhelm.com>
2016-10-24 11:11:33 +0200

    ADDED predeploy-script

--- src/app/scripts.js
+++ src/app/scripts.js
@@ -3,5 +3,6 @@ export default {
   build: 'webpack',
   'build:stats': 'webpack --profile --json > stats.json',
   test: 'tape -r ./test/setup.js \'test/**/*.spec.js\' | tap-spec --color',
+  predeploy: 'npm run build',
   deploy: 'gh-pages -d build',
 };
