36ebe29421f5e3a3c4ddce8001f2dcbb116cab77
Adam Bergman <adam@fransvilhelm.com>
2016-10-27 19:12:23 +0200

    UPDATED lookUpPackageversions
    It now writes a new json-file with new dependecies and versions

--- helpers/lookUpPackageVersions.js
+++ helpers/lookUpPackageVersions.js
@@ -1,4 +1,6 @@
 const { spawn } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 const devDeps = [
   'autoprefixer',
@@ -13,12 +15,12 @@ const devDeps = [
   'babel-plugin-transform-runtime',
   'babel-preset-latest',
   'babel-preset-react',
-  'babel-preset-react-hmre',
   'babel-register',
   'babel-runtime',
   'blue-tape',
-  'chunk-manifest-webpack-plugin',
-  'clean-webpack-plugin',
+  'case-sensitive-paths-webpack-plugin',
+  'chalk',
+  'connect-history-api-fallback',
   'css-loader',
   'css-modules-require-hook',
   'enzyme',
@@ -30,10 +32,11 @@ const devDeps = [
   'eslint-plugin-react',
   'extract-text-webpack-plugin',
   'file-loader',
+  'filesize',
+  'fs-extra',
   'gh-pages',
+  'gzip-size',
   'html-webpack-plugin',
-  'image-webpack-loader',
-  'inline-manifest-webpack-plugin',
   'jsdom',
   'json-loader',
   'node-sass',
@@ -41,8 +44,13 @@ const devDeps = [
   'postcss-loader',
   'promise',
   'react-addons-test-utils',
+  'react-dev-utils',
+  'react-hot-loader',
+  'recursive-readdir',
   'resolve-url-loader',
+  'rimraf',
   'sass-loader',
+  'strip-ansi',
   'style-loader',
   'stylelint',
   'stylelint-config-standard',
@@ -52,9 +60,6 @@ const devDeps = [
   'webpack',
   'webpack-dev-server',
   'webpack-manifest-plugin',
-  'webpack-md5-hash',
-  'webpack-merge',
-  'webpack-validator',
   'whatwg-fetch',
 ];
 
@@ -82,31 +87,32 @@ const mapFn = ((pkg) => new Promise((resolve, reject) => {
 const devDepsPromises = devDeps.sort().map(mapFn);
 const depsPromises = deps.sort().map(mapFn);
 
-Promise.all(devDepsPromises)
+Promise.all([...devDepsPromises, ...depsPromises])
   .then((result) => {
     const obj = result.reduce((prev, curr) => ({
       ...prev,
       [curr.pkg]: `^${curr.version}`,
     }), {});
 
-    console.log('devDependencies');
-    console.log(obj);
+    return obj;
   })
-  .catch((err) => {
-    console.log('Error!');
-    console.log(err);
-  });
+  .then((obj) => new Promise((resolve, reject) => {
+    const newObj = Object.keys(obj).reduce((prev, curr) => {
+      if (curr === 'react' || curr === 'react-dom') {
+        prev.dependencies[curr] = obj[curr]; // eslint-disable-line no-param-reassign
+      } else {
+        prev.devDependencies[curr] = obj[curr]; // eslint-disable-line no-param-reassign
+      }
 
-Promise.all(depsPromises)
-  .then((result) => {
-    const obj = result.reduce((prev, curr) => ({
-      ...prev,
-      [curr.pkg]: `^${curr.version}`,
-    }), {});
+      return prev;
+    }, { dependencies: {}, devDependencies: {} });
 
-    console.log('dependencies');
-    console.log(obj);
-  })
+    fs.writeFile(path.join(__dirname, '..', 'src/app/dependencies.json'), JSON.stringify(newObj, null, 2), (err) => {
+      if (err) reject(err);
+      console.log('Done!');
+      resolve('done');
+    });
+  }))
   .catch((err) => {
     console.log('Error!');
     console.log(err);
