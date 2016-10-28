4566d63d8528e534ddb21a693418f5fc2caac8a3
Adam Bergman <adam@fransvilhelm.com>
2016-10-28 13:43:28 +0200

    UPDATED boilerplate

--- src/app/templates/test/components/App.spec.js
+++ src/app/templates/test/components/App.spec.js
@@ -6,10 +6,20 @@ import App from '../../src/components/App';
 import styles from '../../src/components/App/styles.scss';
 
 test('Component: <App />', (t) => {
-  const wrapper = shallow(<App name="world" />);
+  const wrapper = shallow(<App />);
 
   {
-    const should = 'Should render a <h1>-tag';
+    const should = 'Should initially render an .input-tag';
+    const actual = wrapper.find(`.${styles.input}`).length;
+    const expected = 1;
+
+    t.equal(actual, expected, should);
+  }
+
+  {
+    wrapper.setState({ user: { login: 'test', avatar_url: 'http://test.com/test.jpg' } });
+
+    const should = 'Should render a .header-tag after setState';
     const actual = wrapper.find(`.${styles.header}`).length;
     const expected = 1;
 
