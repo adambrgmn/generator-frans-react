086313ede61a9b0bcc0b87cc30e474c69ecf6020
Adam Bergman <adam@fransvilhelm.com>
2016-10-29 13:18:45 +0200

    ADDED watch-script to watch tests

--- src/app/scripts.js
+++ src/app/scripts.js
@@ -2,6 +2,7 @@ export default {
   start: 'node webpack/start.js',
   build: 'node webpack/build.js',
   test: 'tape -r ./test/setup.js \'test/**/*.spec.js\' | tap-spec --color',
+  watch: 'npm-watch',
   predeploy: 'npm run build',
   deploy: 'gh-pages -d build',
 };
